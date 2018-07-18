3750a2c9414d23307508636ee5fb6463f5e97a14
tedder <ted@timmons.me>
2018-07-18 09:56:12 -0700

    redir to null

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-jsonlint --strict htdocs/phrases.json 
+jsonlint htdocs/phrases.json  >> /dev/null # only want it for a nonzero exit
 sed '/__PHRASES__/ r htdocs/phrases.json' htdocs/index.html > htdocs/_index.html
 aws --profile pjnet s3 cp htdocs/_index.html s3://nimbingo/index.html --acl public-read --cache-control "max-age=6000,public"
 #aws --profile pjnet s3 cp htdocs/phrases.json s3://nimbingo/phrases.json --acl public-read --cache-control "max-age=36000,public" --encoding "UTF-8"
